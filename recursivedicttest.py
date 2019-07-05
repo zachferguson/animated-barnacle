a6263c952809a13203a9c00aad18018cc0f98468
zachferguson <z@thezachferguson.com>
2019-07-05 15:37:29 -0400

    recursive local dictionary builder works

--- recursivedicttest.py
+++ recursivedicttest.py
@@ -7,27 +7,51 @@ from ftplib import FTP_TLS
 import datetime
 import re
 
-    
+comp_dict = {'server_files': {}, 'local_files': {}}
+ignore = [".git"]
 
-def updater(directory, spacing):
+
+def local_parser(directory, spacing):
+    if os.path.split(directory)[1] in ignore:
+        return
     os.chdir(directory)  # change to directory passed in as arguement
     contents = os.listdir(directory)  # list contents of this directory
+
+    if directory not in comp_dict['local_files'].keys():
+        #comp_dict['local_files'].update({directory:{}})
+        comp_dict['local_files'][directory] = {}
     
     for item in contents:  # for file or folder in current directory
-        os.chdir(directory)  # change to arguement directory (in case that has returned back a level from a recursive run
-        itempath = pathlib.Path.cwd().joinpath(item)  # 
-        #print(spacing * " " + f"{item} is a directory: {os.path.isdir(itempath)}")
+        os.chdir(directory)  # change to argument directory (in case that has returned back a level from a recursive run
+        itempath = str(pathlib.Path.cwd().joinpath(item))  # 
         print(spacing * " " + str(itempath))
         print(" ")
         if not path.isdir(itempath):
             print(spacing * " " + item + " is a file")
+            #temp = Local_File(item, datetime.datetime.fromtimestamp(os.path.getmtime(itempath)).strftime('%Y-%m-%d %H:%M:%S'), itempath)
+            temp = {'filename':str(item), 'mod':datetime.datetime.fromtimestamp(os.path.getmtime(itempath)).strftime('%Y-%m-%d %H:%M:%S'), 'filepath':itempath}
+            #comp_dict['local_files'][directory].update({item:temp})
+            #comp_dict['local_files'][directory].update(json.dumps(temp))
+            comp_dict['local_files'][directory][item] = temp
         else:
             print(spacing * " " + item + " is a directory")
             print(spacing * " " + f"_______new_directory: {item}_______")
-            updater(itempath, spacing + 2)
+            local_parser(itempath, spacing + 2)
             #print(itempath)
             #print(os.listdir(itempath))
             print(spacing * " " + f"_______end_directory: {item}_______")
+    
 
-
-updater(os.getcwd(), 0)
+local_parser(os.getcwd(), 0)
+print("******* RESULTS *******")
+for entry in comp_dict['local_files']:
+    print(entry)
+    print(comp_dict['local_files'][entry])
+    '''
+    for item in entry:
+        #print(json.dumps(item))
+        print(item)
+        #print(comp_dict['local_files'][entry][item])
+        print("*******")
+    '''
+    print("_____________________________")
